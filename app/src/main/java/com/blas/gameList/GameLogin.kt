890b8b30fc57c895d2188629f42c18e5af621e0e
Blas <silentnightmar3@gmail.com>
2022-02-16 15:51:12 +0100

    Agregada ventana login fea. Y subida imagen para el login

---
+++ app/src/main/java/com/blas/gameList/GameLogin.kt
@@ -0,0 +1,44 @@
+package com.blas.gameList
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+
+@Composable
+fun GameLogin(
+    loginclick: () -> Unit
+) {
+    var user by remember{mutableStateOf("")}
+    var password by remember{mutableStateOf("")}
+
+    Card(elevation = 5.dp) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 15.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+            Text("User", style = MaterialTheme.typography.h6)
+            TextField(value = user, onValueChange = {user = it})
+            Text("Password", style = MaterialTheme.typography.h6)
+            TextField(value = password, onValueChange = {password = it})
+
+            Button(onClick = {
+                if ( user.equals("blas") && password.equals("blas2020")){
+                    loginclick()
+                }
+            }) {
+                Text(text = "Login")
+            }
+            
+            //Image(painter = rememberImagePainter(request = ), contentDescription = )
+        }
+    }
+}
--- app/src/main/java/com/blas/gameList/NavigationHost.kt
+++ app/src/main/java/com/blas/gameList/NavigationHost.kt
@@ -1,6 +1,7 @@
 package com.blas.gameList
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -12,7 +13,11 @@ import com.blas.gameList.data.DataProvider
 fun NavigationHost() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Destinations.gameList.route){
+    NavHost(navController = navController, startDestination = Destinations.GameLogin.route){
+        composable(Destinations.GameLogin.route) {
+            GameLogin(){
+                navController.navigate(Destinations.gameList.route)           }
+        }
         composable(Destinations.gameList.route) {
             GameList(
                 DataProvider.gameList

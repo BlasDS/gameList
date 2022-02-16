a100516d6c40c288649afb0b91ee04c8849068d9
Blas <silentnightmar3@gmail.com>
2022-02-16 16:38:02 +0100

    Arreglado logo del login.

--- app/src/main/java/com/blas/gameList/GameLogin.kt
+++ app/src/main/java/com/blas/gameList/GameLogin.kt
@@ -1,9 +1,7 @@
 package com.blas.gameList
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -18,27 +16,54 @@ fun GameLogin(
     var user by remember{mutableStateOf("")}
     var password by remember{mutableStateOf("")}
 
-    Card(elevation = 5.dp) {
+    Card(
+        elevation = 5.dp,
+        modifier = Modifier.fillMaxHeight()
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(all = 15.dp),
+                .padding(all = 0.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             ) {
+            Image(
+                painter = rememberImagePainter(data = "https://github.com/BlasDS/gameList/blob/master/app/src/main/res/logo/GameKOLogo.png?raw=true"),
+                contentDescription = "logo",
+                modifier = Modifier
+                    .fillMaxHeight(0.2f)
+                    .fillMaxWidth(0.5f)
+                    .padding(5.dp)
+            )
             Text("User", style = MaterialTheme.typography.h6)
             TextField(value = user, onValueChange = {user = it})
-            Text("Password", style = MaterialTheme.typography.h6)
-            TextField(value = password, onValueChange = {password = it})
+            Text("Password",
+                style = MaterialTheme.typography.h6
+            )
+            TextField(
+                value = password,
+                onValueChange = {password = it},
+            )
 
-            Button(onClick = {
-                if ( user.equals("blas") && password.equals("blas2020")){
+            Button(
+                onClick = {
+                if ( user.equals("blas") && password.equals("blas2020")) {
                     loginclick()
                 }
             }) {
-                Text(text = "Login")
+                Text(
+                    text = "Login",
+                    modifier = Modifier
+                        .padding(horizontal = 104.dp)
+                )
             }
-            
-            Image(painter = rememberImagePainter(data = "https://github.com/BlasDS/gameList/blob/master/app/src/main/res/logo/GameKOLogo.png?raw=true"), contentDescription = "logo")
+            Image(
+                painter = rememberImagePainter(data = "https://github.com/BlasDS/gameList/blob/master/app/src/main/res/logo/GameKOLogo.png?raw=true"),
+                contentDescription = "logo",
+                modifier = Modifier
+                    .fillMaxHeight(0.2f)
+                    .fillMaxWidth(0.5f)
+                    .padding(5.dp)
+            )
         }
     }
 }
